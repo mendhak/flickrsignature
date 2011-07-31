89580342abab722b0e77b98f730a0f05c7caa555
Mendhak <smaltaf@msn.com>
2011-07-31 19:47:23 +0100

    Removed flickrpy, too slow, used native methods instead

--- index.py
+++ index.py
@@ -1,14 +1,15 @@
 from urlparse import urlparse
 from mod_python import Cookie, util
 import re
-import flickr
 import time
 import urllib
 from xml.dom import minidom
 
 
+
 def index(req):
 
+	apiKey = "a39dfdf51784c76fa3234f88bec38b0e"
 	qs = urlparse(req.subprocess_env['QUERY_STRING'])
 	
 	nsid = ''
@@ -31,26 +32,18 @@ def index(req):
 			if 'item' in params and len(params['item']) > 0:
 				item = params['item']
 
-			nsid = getNSID(req, params)
+			nsid = getNSID(req, apiKey, params['user'])
 			
-			#Now get the photos
-			photos = getPhoto(nsid, num)
-
-			if len(photos) > 0:
-				selectedPhoto = photos.pop(0)
-				
+			photo = getPhoto(apiKey, nsid, num)
+			
+			if photo:
 				if item == 'img':
-					redirectToImage(selectedPhoto, size, req)
+					redirectToImage(photo, size, req)
 				else:
-					redirectToPhotoPage(selectedPhoto, nsid, req)
+					redirectToPhotoPage(photo, nsid, req)
 					
 			
 	return 'No parameters supplied'
-
-
-def getPhoto(nsid, photoNumber):
-	return flickr.photos_search(user_id=nsid, per_page=1, page=photoNumber)
-	
 
 
 def redirectToImage(selectedPhoto, size, req):
@@ -75,19 +68,44 @@ def redirectToPhotoPage(selectedPhoto, nsid, req):
 	util.redirect(req, location=photoPage, permanent=False) 
 
 
-def getNSID(req, params):
+def getPhoto(apiKey, nsid, photoNumber):
+
+	photoSearchUrl = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={0}&user_id={1}&per_page=1&page={2}".format(apiKey, nsid, photoNumber)
+	dom = minidom.parse(urllib.urlopen(photoSearchUrl))
+
+	photoNode = dom.getElementsByTagName("photo")[0] 
+	photo = FlickrPhoto()
+	photo.id = photoNode.getAttribute("id")
+	photo.server = photoNode.getAttribute("server")
+	photo.farm = photoNode.getAttribute("farm")
+	photo.secret = photoNode.getAttribute("secret")
+	return photo
+
+def getNSID(req, apiKey, username):
 	cookies = Cookie.get_cookies(req)
-	cookieKey = 'nsid_' + params['user']
+	cookieKey = 'nsid_' + username
 
 	if cookies.has_key(cookieKey):
 		nsid = cookies[cookieKey].value
-	elif not re.match("([0-9]+@N[0-9]+)", params['user']) and not cookieKey in cookies:
-		nsid = flickr.getUserNSID(params['user'])
+	elif not re.match("([0-9]+@N[0-9]+)", username) and not cookieKey in cookies:
+		lookupUrl = "http://api.flickr.com/services/rest/?method=flickr.urls.lookupUser&api_key={0}&url=www.flickr.com/photos/{1}".format(apiKey, username)
+		dom = minidom.parse(urllib.urlopen(lookupUrl))
+		userNode = dom.getElementsByTagName("user")[0]
+		nsid = userNode.getAttribute("id")
 		c = Cookie.Cookie(cookieKey, nsid)
 		c.expires = time.time() + 30 * 24 * 60 * 60
 		Cookie.add_cookie(req, c)
 	else:
-		nsid = params['user']
+		nsid = username
 					
 	return nsid
+
+
+
+class FlickrPhoto:
+	id = ''
+	server = ''
+	farm = ''
+	secret = ''
+
 
