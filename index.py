57b659b53415e38dce73f6e46a0a0b32d84857af
Mendhak <smaltaf@msn.com>
2011-07-31 19:09:56 +0100

    (Unoptimized) Gets images and URLs now, with test page

--- index.py
+++ index.py
@@ -1,13 +1,14 @@
 from urlparse import urlparse
-from mod_python import Cookie
+from mod_python import Cookie, util
 import re
 import flickr
 import time
+import urllib
+from xml.dom import minidom
+
 
 def index(req):
 
-
-	req.content_type="text/html"
 	qs = urlparse(req.subprocess_env['QUERY_STRING'])
 	
 	nsid = ''
@@ -21,6 +22,8 @@ def index(req):
 			user = params['user']
 			if 'num' in params and len(params['num']) > 0:
 				num = params['num']
+				if num <= 0:
+					num = 1
 	
 			if 'size' in params and len(params['size']) > 0:
 				size = params['size']
@@ -29,9 +32,47 @@ def index(req):
 				item = params['item']
 
 			nsid = getNSID(req, params)
-			return nsid
+			
+			#Now get the photos
+			photos = getPhoto(nsid, num)
+
+			if len(photos) > 0:
+				selectedPhoto = photos.pop(0)
+				
+				if item == 'img':
+					redirectToImage(selectedPhoto, size, req)
+				else:
+					redirectToPhotoPage(selectedPhoto, nsid, req)
+					
 			
 	return 'No parameters supplied'
+
+
+def getPhoto(nsid, photoNumber):
+	return flickr.photos_search(user_id=nsid, per_page=1, page=photoNumber)
+	
+
+
+def redirectToImage(selectedPhoto, size, req):
+	size = size.lower()
+
+	if size == 'm' or size == 'medium' or size == 'med':
+		size = 'm'
+	elif size == 's' or size == 'small':
+		size = 's'
+	elif size == 't' or size == 'thumb' or size == 'thumbnail' or size == 'tiny':
+		size = 't'
+	elif size == 'z' or size == 'medium640' or size == 'medium 640':
+		size = 'z'
+	elif size == 'b' or size == 'large' or size == 'big':
+		size = 'b'
+
+	photoUrl = "http://farm{0}.static.flickr.com/{1}/{2}_{3}_{4}.jpg".format(selectedPhoto.farm, selectedPhoto.server, selectedPhoto.id, selectedPhoto.secret, size)
+	util.redirect(req, location=photoUrl, permanent=False)
+
+def redirectToPhotoPage(selectedPhoto, nsid, req):
+	photoPage = "http://www.flickr.com/photos/{0}/{1}".format(nsid, selectedPhoto.id)
+	util.redirect(req, location=photoPage, permanent=False) 
 
 
 def getNSID(req, params):
@@ -46,9 +87,7 @@ def getNSID(req, params):
 		c.expires = time.time() + 30 * 24 * 60 * 60
 		Cookie.add_cookie(req, c)
 	else:
-		nsid = params['nsid']
+		nsid = params['user']
 					
 	return nsid
-
-
 
