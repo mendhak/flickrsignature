86c339c3ae27cdb5a56437747a450c6d984b0914
Mendhak <smaltaf@msn.com>
2012-04-21 09:33:46 +0100

    Using spaces instead of tabs for indents

--- flickr/flickrapi.py
+++ flickr/flickrapi.py
@@ -5,113 +5,113 @@ from xml.dom import minidom
 
 def getPhoto(apiKey, nsid, photoNumber, popular):
 
-	popular = popular.lower()
+    popular = popular.lower()
 
-	if popular == 'p':
-		popular = 'interestingness-desc'
-	else:
-		popular = 'date-posted-desc'
+    if popular == 'p':
+        popular = 'interestingness-desc'
+    else:
+        popular = 'date-posted-desc'
 
-	photoSearchUrl = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={0}&user_id={1}&per_page=1&page={2}&sort={3}".format(apiKey, nsid, photoNumber, popular)
-	dom = minidom.parse(urllib.urlopen(photoSearchUrl))
+    photoSearchUrl = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={0}&user_id={1}&per_page=1&page={2}&sort={3}".format(apiKey, nsid, photoNumber, popular)
+    dom = minidom.parse(urllib.urlopen(photoSearchUrl))
 
-	photoNode = dom.getElementsByTagName("photo")[0] 
-	photo = FlickrPhoto()
-	photo.id = photoNode.getAttribute("id")
-	photo.server = photoNode.getAttribute("server")
-	photo.farm = photoNode.getAttribute("farm")
-	photo.secret = photoNode.getAttribute("secret")
-	photo.title = photoNode.getAttribute("title")
-	return photo
+    photoNode = dom.getElementsByTagName("photo")[0]
+    photo = FlickrPhoto()
+    photo.id = photoNode.getAttribute("id")
+    photo.server = photoNode.getAttribute("server")
+    photo.farm = photoNode.getAttribute("farm")
+    photo.secret = photoNode.getAttribute("secret")
+    photo.title = photoNode.getAttribute("title")
+    return photo
 
 
 def getPhotoBySearch(apiKey, nsid, searchTag, photoNumber):
-	photoSearchUrl = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={0}&user_id={1}&per_page=1&page={2}&tags={3}".format(apiKey, nsid, photoNumber, searchTag)
+    photoSearchUrl = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={0}&user_id={1}&per_page=1&page={2}&tags={3}".format(apiKey, nsid, photoNumber, searchTag)
 
-	dom = minidom.parse(urllib.urlopen(photoSearchUrl))
+    dom = minidom.parse(urllib.urlopen(photoSearchUrl))
 
-	photoNode = dom.getElementsByTagName("photo")[0]
-	photo = FlickrPhoto()
-	photo.id = photoNode.getAttribute("id")
-	photo.server = photoNode.getAttribute("server")
-	photo.farm = photoNode.getAttribute("farm")
-	photo.secret = photoNode.getAttribute("secret")
-	photo.title = photoNode.getAttribute("title")
-	return photo
+    photoNode = dom.getElementsByTagName("photo")[0]
+    photo = FlickrPhoto()
+    photo.id = photoNode.getAttribute("id")
+    photo.server = photoNode.getAttribute("server")
+    photo.farm = photoNode.getAttribute("farm")
+    photo.secret = photoNode.getAttribute("secret")
+    photo.title = photoNode.getAttribute("title")
+    return photo
 
 
 def getNSID(apiKey, username):
 
-	if not 'http://' in username:
-		username = 'http://www.flickr.com/photos/' + username
+    if not 'http://' in username:
+        username = 'http://www.flickr.com/photos/' + username
 
-	if not re.match("([0-9]+@N[0-9]+)", username):
-		lookupUrl = "http://api.flickr.com/services/rest/?method=flickr.urls.lookupUser&api_key={0}&url={1}".format(apiKey, username)
-		dom = minidom.parse(urllib.urlopen(lookupUrl))
-		userNode = dom.getElementsByTagName("user")[0]
-		nsid = userNode.getAttribute("id")
-	else:
-		nsid = username
+    if not re.match("([0-9]+@N[0-9]+)", username):
+        lookupUrl = "http://api.flickr.com/services/rest/?method=flickr.urls.lookupUser&api_key={0}&url={1}".format(apiKey, username)
+        dom = minidom.parse(urllib.urlopen(lookupUrl))
+        userNode = dom.getElementsByTagName("user")[0]
+        nsid = userNode.getAttribute("id")
+    else:
+        nsid = username
 
-	return nsid
+    return nsid
 
 
 
 def getPhotoPageUrl(selectedPhoto, nsid):
-	if nsid:
-		return "http://www.flickr.com/photos/{0}/{1}".format(nsid, selectedPhoto.id)
-	else:
-		return "http://www.flickr.com/photo.gne?id=" + selectedPhoto.id
+    if nsid:
+        return "http://www.flickr.com/photos/{0}/{1}".format(nsid, selectedPhoto.id)
+    else:
+        return "http://www.flickr.com/photo.gne?id=" + selectedPhoto.id
 
 
 
 
 def getImageUrl(selectedPhoto, size):
 
-	size = size.lower()
-	sizePrefix = '_'
+    size = size.lower()
+    sizePrefix = '_'
 
 
-	'''
-	s	small square 75x75 *
-	q	large square 150x150 *
-	t	thumbnail, 100 on longest side *
-	m	small, 240 on longest side *
-	n	small, 320 on longest side *
-	-	medium, 500 on longest side *
-	z	medium 640, 640 on longest side *
-	c	medium 800, 800 on longest side *
-	b	large, 1024 on longest side *
-	'''
+    '''
+    s	small square 75x75 *
+    q	large square 150x150 *
+    t	thumbnail, 100 on longest side *
+    m	small, 240 on longest side *
+    n	small, 320 on longest side *
+    -	medium, 500 on longest side *
+    z	medium 640, 640 on longest side *
+    c	medium 800, 800 on longest side *
+    b	large, 1024 on longest side *
+    '''
 
-	if size == 'small' or size == '240':
-		size = 'm'
-	if size == 'small320' or size == '320':
-		size = 'n'
-	elif size == 'square' or size == '75':
-		size = 's'
-	elif size == 'largesquare' or size == '150':
-		size = 'q'
-	elif size == 'thumb' or size == 'thumbnail' or size == 'tiny' or size == '100':
-		size = 't'
-	elif size == 'medium640' or size == '640':
-		size = 'z'
-	elif size == 'medium800' or size == '800':
-		size = 'c'
-	elif size == 'large' or size == 'big' or size == '1024':
-		size = 'b'
-	elif size == '' or size == 'medium' or size == 'med' or size == 'x' or size == '500':
-		sizePrefix =  ''
-		size = ''
+    if size == 'small' or size == '240':
+        size = 'm'
+    if size == 'small320' or size == '320':
+        size = 'n'
+    elif size == 'square' or size == '75':
+        size = 's'
+    elif size == 'largesquare' or size == '150':
+        size = 'q'
+    elif size == 'thumb' or size == 'thumbnail' or size == 'tiny' or size == '100':
+        size = 't'
+    elif size == 'medium640' or size == '640':
+        size = 'z'
+    elif size == 'medium800' or size == '800':
+        size = 'c'
+    elif size == 'large' or size == 'big' or size == '1024':
+        size = 'b'
+    elif size == '' or size == 'medium' or size == 'med' or size == 'x' or size == '500':
+        sizePrefix =  ''
+        size = ''
 
-	size = sizePrefix + size
+    size = sizePrefix + size
 
-	return "http://farm{0}.static.flickr.com/{1}/{2}_{3}{4}.jpg".format(selectedPhoto.farm, selectedPhoto.server, selectedPhoto.id, selectedPhoto.secret, size)
+    return "http://farm{0}.static.flickr.com/{1}/{2}_{3}{4}.jpg".format(selectedPhoto.farm, selectedPhoto.server, selectedPhoto.id, selectedPhoto.secret, size)
 
 
 class FlickrPhoto:
-	id = ''
-	server = ''
-	farm = ''
-	secret = ''
-	title = ''
+    id = ''
+    server = ''
+    farm = ''
+    secret = ''
+    title = ''
--- flickr/views.py
+++ flickr/views.py
@@ -15,103 +15,100 @@ apiKey = "a39dfdf51784c76fa3234f88bec38b0e"
 
 def image(request, nsid, num=1, size='', popular=''):
 
-	if not num or not num.isdigit() or int(num) <= 0:
-		num = 1
-	if not size:
-		size = 's'
-	if not popular :
-		popular = ''
+    if not num or not num.isdigit() or int(num) <= 0:
+        num = 1
+    if not size:
+        size = 's'
+    if not popular :
+        popular = ''
 
-
-
-
-	resp = HttpResponse(status=302)
-	nsid = getUserNSID(request, resp, apiKey, nsid)
-	photo = flickrapi.getPhoto(apiKey, nsid, num, popular)
-	destinationUrl = flickrapi.getImageUrl(photo, size)
-	resp['Cache-Control'] = "private, max-age=3600"	
-	resp['Location'] = destinationUrl
-	return resp
+    resp = HttpResponse(status=302)
+    nsid = getUserNSID(request, resp, apiKey, nsid)
+    photo = flickrapi.getPhoto(apiKey, nsid, num, popular)
+    destinationUrl = flickrapi.getImageUrl(photo, size)
+    resp['Cache-Control'] = "private, max-age=3600"
+    resp['Location'] = destinationUrl
+    return resp
 
 def searchImage(request, tags='', num=1, size='', nsid='' ):
-	if not num or not num.isdigit() or int(num) <= 0:
-		num = 1
-	if not size:
-		size = 's'
+    if not num or not num.isdigit() or int(num) <= 0:
+        num = 1
+    if not size:
+        size = 's'
 
-	resp = HttpResponse(status=302)
-	nsid = getUserNSID(request, resp, apiKey, nsid)
-	photo = flickrapi.getPhotoBySearch(apiKey, nsid, tags, num)
-	destinationUrl = flickrapi.getImageUrl(photo, size)
-	resp['Cache-Control'] = "private, max-age=3600"
-	resp['Location'] = destinationUrl
-	return resp
+    resp = HttpResponse(status=302)
+    nsid = getUserNSID(request, resp, apiKey, nsid)
+    photo = flickrapi.getPhotoBySearch(apiKey, nsid, tags, num)
+    destinationUrl = flickrapi.getImageUrl(photo, size)
+    resp['Cache-Control'] = "private, max-age=3600"
+    resp['Location'] = destinationUrl
+    return resp
 
 def searchRedirect(request, tags='', num=1, nsid=''):
-	if not num or not num.isdigit() or int(num) <= 0:
-		num = 1
-	resp = HttpResponse(status=302)
-	nsid = getUserNSID(request, resp, apiKey, nsid)
-	photo = flickrapi.getPhotoBySearch(apiKey, nsid, tags, num)
-	destinationUrl = flickrapi.getPhotoPageUrl(photo, nsid)
-	resp['Location'] = destinationUrl
-	return resp
+    if not num or not num.isdigit() or int(num) <= 0:
+        num = 1
+    resp = HttpResponse(status=302)
+    nsid = getUserNSID(request, resp, apiKey, nsid)
+    photo = flickrapi.getPhotoBySearch(apiKey, nsid, tags, num)
+    destinationUrl = flickrapi.getPhotoPageUrl(photo, nsid)
+    resp['Location'] = destinationUrl
+    return resp
 
 
 def redirect(request, nsid, num, popular):
-	if not num or not num.isdigit() or int(num) <= 0:
-		num = 1
-	if not popular:
-		popular = ''
+    if not num or not num.isdigit() or int(num) <= 0:
+        num = 1
+    if not popular:
+        popular = ''
 
-	resp = HttpResponse(status=302)
-	nsid = getUserNSID(request, resp, apiKey, nsid)
-	photo = flickrapi.getPhoto(apiKey, nsid, num, popular)
-	destinationUrl = flickrapi.getPhotoPageUrl(photo, nsid)
-	resp['Location'] = destinationUrl
-	return resp
+    resp = HttpResponse(status=302)
+    nsid = getUserNSID(request, resp, apiKey, nsid)
+    photo = flickrapi.getPhoto(apiKey, nsid, num, popular)
+    destinationUrl = flickrapi.getPhotoPageUrl(photo, nsid)
+    resp['Location'] = destinationUrl
+    return resp
 
 def nsid(request, username):
-	resp = HttpResponse()
-	nsid = getUserNSID(request, resp, apiKey, username)
-	resp.write(nsid)
-	return resp
+    resp = HttpResponse()
+    nsid = getUserNSID(request, resp, apiKey, username)
+    resp.write(nsid)
+    return resp
 
 def getUserNSID(request, response, apiKey, username):
 
-	if username is None:
-		return ''
+    if username is None:
+        return ''
 
-	if 'http://' in username or 'www.' in username:
-		userRegex = re.compile(r'photos/(?P<username>[^/]+)')
-		m = userRegex.search(username)
-		username = m.group('username')
+    if 'http://' in username or 'www.' in username:
+        userRegex = re.compile(r'photos/(?P<username>[^/]+)')
+        m = userRegex.search(username)
+        username = m.group('username')
 
-	cookies = request.COOKIES
-	cookieKey = str('nsid_' + username)
-	nsidRegex = re.compile("([0-9]+@N[0-9]+)")
+    cookies = request.COOKIES
+    cookieKey = str('nsid_' + username)
+    nsidRegex = re.compile("([0-9]+@N[0-9]+)")
 
-	if cookies.has_key(cookieKey):
-		nsid = cookies[cookieKey]
-	elif not nsidRegex.match(username) and not cookieKey in cookies:
-		nsid = flickrapi.getNSID(apiKey, username)
-		setCookie(response, cookieKey, nsid)
-	else:
-		nsid = username
-	
-	return nsid
+    if cookies.has_key(cookieKey):
+        nsid = cookies[cookieKey]
+    elif not nsidRegex.match(username) and not cookieKey in cookies:
+        nsid = flickrapi.getNSID(apiKey, username)
+        setCookie(response, cookieKey, nsid)
+    else:
+        nsid = username
+
+    return nsid
 
 
 def main(request):
-	return render_to_response('sig.html', {'domain': request.get_host()}, context_instance=RequestContext(request))
+    return render_to_response('sig.html', {'domain': request.get_host()}, context_instance=RequestContext(request))
 
 
 def setCookie(response, cookieKey, nsid, expire=None):
 
-	if expire is None:
-		max_age = 365*24*60*60  
-	else:
-		max_age = expire
-	expires = datetime.datetime.strftime(datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
-	response.set_cookie(key=str(cookieKey), value=nsid, max_age=max_age, expires=expires, domain=None, secure=False)
+    if expire is None:
+        max_age = 365*24*60*60
+    else:
+        max_age = expire
+    expires = datetime.datetime.strftime(datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
+    response.set_cookie(key=str(cookieKey), value=nsid, max_age=max_age, expires=expires, domain=None, secure=False)
 
